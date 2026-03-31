ee2b755bc834edd36992edebd57dac33999bda57
shawon <saifulislamshawon1234@gmail.com>
2026-03-31 17:44:26 +0600

    first comment

---
+++ __manifest__.py
@@ -0,0 +1,45 @@
+{
+    'name': 'Arabian_website',
+    'description': 'This is i make for learning purpose',
+    'category': 'Theme/Creative',
+    'version': '1.0',
+    'author': 'Saiful Islam Shawon',
+    'depends': ['website'],
+    'data': [
+        'views/theme.xml',
+        'views/homepage.xml',
+        'views/footer.xml',
+        'views/header.xml',
+        'views/snippets/snippets.xml',
+        'views/snippets/property_agent.xml',
+       
+    ],
+    'assets': {
+         # Theme variables
+        "web._assets_primary_variables": [
+            "arabian_website/static/src/scss/primary_variables.scss",
+        ],
+        
+        
+        # 🟢 Frontend Assets
+        'web.assets_frontend': [
+
+            # ✅ External CSS
+            'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css',
+            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+            "arabian_website/static/src/scss/styles.scss",
+
+            # ✅ JS Library (Swiper MUST before custom JS)
+            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+
+            # ✅ Custom JS
+            'arabian_website/static/src/js/slider.js',
+        ],
+    },
+    'images': [],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'theme': True,
+    'license': 'LGPL-3',
+}
